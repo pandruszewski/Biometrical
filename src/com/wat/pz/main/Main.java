83839a8590b99badcf9d77252b22556bcdc34d58
Przemek <przemek@natan.pl>
2012-12-01 01:14:27 +0100

    -zmienione gui lekko ;)

--- src/com/wat/pz/main/Main.java
+++ src/com/wat/pz/main/Main.java
@@ -1,11 +1,26 @@
 package com.wat.pz.main;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 
 public class Main {
 
 	public static void main(String[] args){
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception ee) {
+			// If Nimbus is not available, you can set the GUI to another look
+			// and feel.
+		}
 		Window window = new Window();
 
 		window.setSize(500, 500);
+		
 	}
 }
